71ded1cd069ca83bd12f6a11345ca93362579b1e
Elizabethka409 <133019223+Elizabethka409@users.noreply.github.com>
2025-05-13 15:36:32 -0400

    Add files via upload

---
+++ MyFirstProgram.java
@@ -0,0 +1,15 @@
+/*    
+   Chapter 1
+   Prompt: Everyone's first program
+   Author: Elizabeth Ka
+   Date: May 13th, 2025
+*/
+
+public class MyFirstProgram
+{
+   public static void main(String[] args)
+   {
+      // This is how you display text (output) to the user in Java:
+      System.out.println("Hello World!!!");
+   }
+}
